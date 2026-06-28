a4a4eca4c7adaf371d7cdc150e8cd9a59c03ff68
anYuJia <2259202040@qq.com>
2026-06-28 16:13:56 +0800

    refactor(media): 收敛媒体代理入口模块

--- src-tauri/src/media_proxy.rs
+++ src-tauri/src/media_proxy.rs
@@ -1,52 +1,15 @@
-use crate::config::get_user_agent;
 use crate::AppState;
-use crate::media_proxy_crypto::{decrypt_im_image_bytes, guess_image_content_type_from_bytes};
-use axum::body::{Body, Bytes};
-use axum::extract::{Query, State};
-use axum::http::{header, HeaderMap, HeaderValue, Response, StatusCode};
 use axum::routing::get;
 use axum::Router;
-use futures::StreamExt;
-use serde::Deserialize;
 use std::net::SocketAddr;
-use std::path::{Path, PathBuf};
-use tokio::io::{AsyncReadExt, AsyncSeekExt};
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
-use url::Url;
 
-use crate::media_proxy_headers::{apply_cors_headers, build_error_response};
-use crate::media_proxy_security::{
-    allowed_request_origin, guess_content_type, is_allowed_media_url, media_url_label,
-    resolve_redirect_target, should_send_cookie,
-};
 pub(crate) use crate::media_proxy_cache::CachedMediaRange;
-use crate::media_proxy_cache::{
-    cap_remote_media_range, parse_byte_range,
-    remote_media_range_cache_keys, cached_media_response,
-    LOCAL_MEDIA_INITIAL_RANGE_BYTES, LOCAL_MEDIA_MAX_RANGE_BYTES,
-    REMOTE_MEDIA_MAX_RANGE_BYTES, REMOTE_MEDIA_RANGE_CACHE_ENTRIES,
-};
 use crate::media_proxy_local::{local_media, frontend_dist_dir};
 use crate::media_proxy_remote::{media_proxy, media_proxy_options, seek_debug};
 
 pub const MEDIA_PROXY_PORT: u16 = 39143;
-const INITIAL_VIDEO_RANGE: &str = "bytes=0-1048575";
-const PREWARM_HEADER: &str = "x-douyin-prewarm";
-const MAX_RETRIES: usize = 3;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 pub async fn spawn_media_proxy(state: AppState) -> anyhow::Result<()> {
     let addr = SocketAddr::from(([127, 0, 0, 1], MEDIA_PROXY_PORT));
@@ -77,5 +40,3 @@ pub async fn spawn_media_proxy(state: AppState) -> anyhow::Result<()> {
 
     Ok(())
 }
-
-
